b87b8d60e509581c49b0fdb652f0f99c37c41028
BlakeAnd <mblakeandrew@gmail.com>
2019-10-18 08:31:35 -0700

    got the ring buffer working, passing all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,17 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    self.current += 1
+    if self.current == self.capacity:
+      self.current = 0
 
   def get(self):
-    pass
+    stop_point = self.capacity
+    for i in range(0, self.capacity):
+      if self.storage[i] == None:
+        stop_point = i
+    #     print("i", i)
+
+    # print(self.storage[:stop_point])
+    return self.storage[:stop_point]
